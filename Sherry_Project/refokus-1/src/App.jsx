896ec37873644fe5e92a5bc185fc5a1714a7fae7
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-18 21:35:54 +0530

    updateed navbar

--- Sherry_Project/refokus-1/src/App.jsx
+++ Sherry_Project/refokus-1/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 function App() {
   return (
     <>
-    <div className=' p-1 min-h-screen min-w-screen text-zinc-100 bg-zinc-700'>
+    <div className=' p-1 min-h-screen min-w-screen text-zinc-100 bg-zinc-800'>
  <Navbar></Navbar>      
  
     </div>
