60f2ea79326e0c6aafe97601f4a6ad891c7fea0f
Nitesh-DK <nittesh.dk@gmail.com>
2024-10-26 19:57:10 +0530

    all done

---
+++ Practice_Projects/react-project-eyes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+    <div className='min-h-screen min-w-screen bg-zinc-700'>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
+
