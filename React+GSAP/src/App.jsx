4213acf71ff63e9023f359c612fe3af7b38f166f
nitesh11-dk <nittesh.dk@gmail.com>
2024-12-08 21:23:26 +0530

    Add GSAP and @gsap/react for animations, refactor App to use Project component

--- React+GSAP/src/App.jsx
+++ React+GSAP/src/App.jsx
@@ -1,35 +1,13 @@
-import React, { useRef } from 'react';
+import React from 'react'
+import Project from './Project'
 
 const App = () => {
-  const refs = useRef([]);
-  const toggleBackgroundColor = () => {
-    refs.current.forEach((ref) => {
-      if (ref) {
-        ref.style.backgroundColor =
-          ref.style.backgroundColor === 'blue' ? 'red' : 'blue';
-      }
-    });
-  };
-
   return (
-    <div className='w-screen h-screen bg-base-200 justify-center flex items-center flex-col'>
-      <div
-        ref={(el) => (refs.current[0] = el)}
-        className='h-44 w-40 flex items-center justify-center bg-red-500 rounded-xl'>
-        The Box
-      </div>
-      <div
-        ref={(el) => (refs.current[1] = el)}
-        className='h-40 w-40 flex items-center justify-center bg-red-500 rounded-xl'>
-        The Box
-      </div>
-      <button
-        onClick={toggleBackgroundColor}
-        className='mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg'>
-        Toggle Background
-      </button>
-    </div>
-  );
-};
+    <div className='h-screen w-screen bg-base-200'>
+  <Project/>
 
-export default App;
+    </div>
+  )
+}
+
+export default App
