2047b7fb458f0cf13b68c6c6facdd0e2846d7d3e
Nitesh-DK <nittesh.dk@gmail.com>
2025-01-14 11:51:02 +0530

    Enhance Experience component with ContactShadows and additional Women instance
    - Added ContactShadows for improved lighting effects in the 3D scene.
    - Introduced a new instance of the Women component with custom properties for position and colors.

--- R3F/Multiplayer/client/src/Components/Experience.jsx
+++ R3F/Multiplayer/client/src/Components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import React from "react";
 import { Women } from "./3D/Women";
 
@@ -7,7 +7,14 @@ const Experience = () => {
     <>
       <Environment preset="sunset" />
       <OrbitControls />
+      <ContactShadows blur={2}></ContactShadows>
       <Women></Women>
+      <Women
+        position-x={1}
+        hairColor="red"
+        topColor="blue"
+        bottomColor="green"
+      ></Women>
     </>
   );
 };
